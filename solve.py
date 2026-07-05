eca129c24e6146c4d180ee91469d9b886a393401
haribhaski <haribhaski@gmail.com>
2026-07-05 16:46:59 +0530

    Updated optimization using nested

--- solve.py
+++ solve.py
@@ -4,34 +4,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import differential_evolution, minimize, minimize_scalar
 
-
 def model(t, theta, M, X):
     envelope = np.exp(M * np.abs(t)) * np.sin(0.3 * t)
-
     x = t * np.cos(theta) - envelope * np.sin(theta) + X
     y = 42 + t * np.sin(theta) + envelope * np.cos(theta)
-
     return x, y
-
 
 def curve_loss(params, observed_x, observed_y):
     theta_deg, M, X = params
     theta = np.radians(theta_deg)
-
     distances = []
 
     for x_i, y_i in zip(observed_x, observed_y):
-
         def point_distance(t):
             px, py = model(t, theta, M, X)
             return (px - x_i) ** 2 + (py - y_i) ** 2
-
-        result = minimize_scalar(
-            point_distance,
-            bounds=(6, 60),
-            method="bounded"
-        )
-
+            
+        result = minimize_scalar(point_distance,bounds=(6, 60),method="bounded")
         distances.append(np.sqrt(result.fun))
 
     return np.mean(distances)
@@ -44,12 +33,7 @@ df = pd.read_csv("xy_data.csv")
 
 x_obs = df["x"].values
 y_obs = df["y"].values
-
-bounds = [
-    (0, 50),
-    (-0.05, 0.05),
-    (0, 100)
-]
+bounds = [(0, 50),(-0.05, 0.05),(0, 100)]
 
 result = differential_evolution(
     curve_loss,
@@ -88,7 +72,6 @@ with open("results/final_result.txt", "w") as f:
 
 t_dense = np.linspace(6, 60, 2000)
 x_pred, y_pred = model(t_dense, np.radians(theta_deg), M, X)
-
 plt.figure(figsize=(8,6))
 plt.scatter(df["x"],df["y"],s=20,color="red",label="Observed Data")
 plt.plot(x_pred,y_pred,color="blue",linewidth=2,label="Approximated Curve")
@@ -98,10 +81,8 @@ plt.title("Observed vs Approximated Curve")
 plt.axis("equal")
 plt.grid(True)
 plt.legend()
-
 plt.savefig("results/fitted_curve.png", dpi=300, bbox_inches="tight")
 plt.show()
-
 print("Saved:")
 print("results/final_result.txt")
 print("results/fitted_curve.png")
